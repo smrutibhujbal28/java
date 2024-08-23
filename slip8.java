59bd96ce4c91052012790c5cccacaf733acd12a5
smrutibhujbal28 <160315996+smrutibhujbal28@users.noreply.github.com>
2024-08-23 15:42:48 +0530

    Add files via upload

---
+++ slip8.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+class slip8
+{
+public static void main(String args[])
+{
+char color='g';
+switch(color)
+{
+case'r':System.out.println("Red");
+case'g':System.out.println("Green");
+case'b':System.out.println("Blue");
+default:System.out.println("no color");
+}
+}
+}
