5082eb05e40b928c94961a77badeeb52d66b4e7d
smrutibhujbal28 <160315996+smrutibhujbal28@users.noreply.github.com>
2024-08-23 15:43:22 +0530

    Add files via upload

---
+++ slip7.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+class slip7
+{
+public static void main(String args[])
+{
+int i,j;
+int array[][]={{1,3,4},{4,5,6},{8,9,0}};
+System.out.println("Transpose of matrix is:");
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+{
+System.out.print(array[j][i]+"");
+}
+System.out.println("");
+}
+}
+}
+
