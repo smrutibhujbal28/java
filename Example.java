b7a738357d0d2612a26c80d935d0f86d87dc99ee
smrutibhujbal28 <160315996+smrutibhujbal28@users.noreply.github.com>
2024-08-23 15:42:07 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+class  Example
+{
+static int count=0;
+static void p()
+{
+count++;
+if(count<=5)
+{
+System.out.println("hello"+count);
+p();
+}
+}
+public static void main(String args[])
+{
+p();
+}
+}
