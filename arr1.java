330494ea3a8b76b9f3745217b4f30b29d40118da
smrutibhujbal28 <160315996+smrutibhujbal28@users.noreply.github.com>
2024-08-23 15:40:42 +0530

    Add files via upload

---
+++ arr1.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+class arr1
+{
+public static void main(String args[])
+{
+float [] []={{1.1f,1.2f,1.3f},{2.1f,2.2f,2.3f},{3.1f,3.2f,3.3f}};
+System.out.println("In matrix form");
+for(int i=0;i<3;i++)
+{
+for(int j+0;j<3;j++)
+{
+system.out.print(x[i][j]+"\t");
+}
+System.out.println();
+}
+}
+}
